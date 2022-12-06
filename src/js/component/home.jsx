0a5db5a887768020ae85aee1de4dc6f86611717e
LukasOy <lukas.oylu@gmail.com>
2022-12-06 01:22:02 +0000

    01 05-12 Intento Arreglo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,40 +1,62 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+const Caja1CSS = (
+{
+ color: "white",
+ backgroundColor: "black",
+ justifyContent: "center",
+ marginLeft: "100px",
+ textAlign: "center",
+ 
+});
+
+const Caja2CSS =(
+	{
+		paddingTop: "10px",
+		alignItems: "flex-start",
+		backgroundImage: 'url("https://thumbs.dreamstime.com/z/vista-de-cerca-dos-hojas-papel-sobre-las-otras-aisladas-fondo-blanco-en-185331748.jpg")',
+	}
+) ;
 
 const Home = () => {
-	const [tarea, nueva] = useState(["Tarea1","Tarea2","Tarea3",]);
-		
-	const borrar = (b) => {
-		nueva(tarea.filter((value,index,arr)=> {return index != b}));
-	}
- 	
-	return (
-		
-			<div>
-				
-		
-			<div id="Caja2"> </div>
-  			<div id="Caja3"> 
-			  <h3>Lista de Tareas</h3>
+  const [tarea, nueva] = useState(["Tarea1", "Tarea2", "Tarea3"]);
 
-				
-				<form onSubmit={(event)=>{
-					event.preventDefault();
-					nueva([...tarea,event.target[0].value]);
-				}}>
+  const borrar = (b) => {
+    nueva(
+      tarea.filter((value, index, arr) => {
+        return index != b;
+      })
+    );
+  };
 
-				<input placeholder="¿cual es tu tarea?"></input>  
-				<button> Ingresar</button>
-				</form>
-				{tarea.map((value,index, arr)=>{
-					return <li key={index}>{value} 
-					<button onClick={() => borrar(index)}> X </button></li>
-
-				})}
-				<p id="caja4" class="text-muted">Cantidad de tareas {tarea.length}</p>
-				</div>
-			</div>
-	);
-	
+  return (
+    <div style={Caja1CSS}>
+      <div>
+        <h3>Lista de Tareas</h3>
+      </div>
+      <div style={Caja2CSS}>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            nueva([...tarea, event.target[0].value]);
+          }}
+        >
+          <input placeholder="¿cual es tu tarea?"></input>
+          <button> Ingresar</button>
+        </form>
+        {tarea.map((value, index, arr) => {
+          return (
+            <li key={index}>
+              {value}
+              <button onClick={() => borrar(index)}> X </button>
+            </li>
+          );
+        })}
+        <p id="caja4" class="text-muted">
+          Cantidad de tareas {tarea.length}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
